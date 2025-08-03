6ac16faf5ebf7aa267bb7e041e3acc26d03b17a6
Dark Lucifer <nimeshpiyumal444@gmail.com>
2025-08-03 20:55:54 +0530

    Fixed

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '',
+    SESSION_ID: process.env.SESSION_ID || '', 
     PREFIX: process.env.PREFIX || '.',
     OWNER_NUMBER: process.env.OWNER_NUMBER || [''],
     MONGODB_URI: process.env.MONGODB_URI || '',
