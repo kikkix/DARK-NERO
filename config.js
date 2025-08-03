0b2dda91ea4931e7db5f0b6c8d7fa7a43347018e
Dark Lucifer <nimeshpiyumal444@gmail.com>
2025-08-03 20:55:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'GENUX-c7e6f8',
+    SESSION_ID: process.env.SESSION_ID || '',
     PREFIX: process.env.PREFIX || '.',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || ['94764497078', '94764497077'],
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://exekieligop:ul8XnJl05zUDHrEu@sandaru.tc3wtqw.mongodb.net',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || [''],
+    MONGODB_URI: process.env.MONGODB_URI || '',
 };
